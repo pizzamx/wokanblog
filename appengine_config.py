03e02c31820b5a23d7cff16acb27c54ef743d420
unknown <pizzamx@gmail.com>
2014-02-13 23:53:06 +0800

    +stat

--- appengine_config.py
+++ appengine_config.py
@@ -1,5 +1,8 @@
 from google.appengine.ext.appstats import recording
 
+appstats_CALC_RPC_COSTS = True
+appstats_SHELL_OK = True
+
 def webapp_add_wsgi_middleware(app):
     app = recording.appstats_wsgi_middleware(app)
     return app
